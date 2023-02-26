938e041dda497e75e3efa1626031caedd0fec74b
Mariana Ramos <mary.programms@gmail.com>
2023-02-26 19:45:58 +0000

    ternary and array exercise

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import './App.css';
 
 function App() {
-  return (
-  <div className="App">
-    <div>
-      <User name="Mariana" age={24} email="mary.programs@gmail.com" />
-      <User name="Sara" age={30} email="sara.programs@gmail.com" />
-      <User name="Bela" age={38} email="bela.programs@gmail.com" />
-    </div>
+  const users = [
+    {name: "Pedro", age: 21},
+    {name: "Mariana", age: 24},
+    {name: "Hugo", age: 27},
+  ];
 
-    <div>
-      <Job salary={90000} position="Senior SDE" company="Amazon"/>
-      <Job salary={12000} position="Junior SDE" company="Google"/>
-      <Job salary={10000} position="Project Manager" company="Netflix"/>
+  return (
+    <div className="App">
+      {users.map((user, key) => {
+        return (
+          <User />
+        )
+      })}
     </div>
-  </div>
   );
 }
 
 const User = (props) => {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
+      {props.name} {props.age}
     </div>
-  );
-};
-
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.position}</h1>
-      <h2>{props.salary}</h2>
-      <h3>{props.company}</h3>
-    </div>
-  );
-};
+  )
+}
 
 export default App;
