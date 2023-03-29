539011aac3ba74dd6e1fbde135b54d3351362fb8
mary <mary.programms@gmail.com>
2023-03-29 22:39:03 +0100

    project init

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mianaLab</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Oxanium:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <main class="wrapper">
+        <header>
+            <p class="logo">mianaLab</p>
+        </header>
+
+        <div class="content">
+            <div class="container"></div>
+            
+            <!-- HOMEPAGE -->
+            <div class="hero">
+                <div class="hero__shot">
+                    <img src="assets/img/hero-desk.jpg" alt="" class="hero__img">
+                </div>
+                <div class="hero__info">
+                    <h1 class="hero__title">Hey, welcome to mianaLab!</h1>
+                    <p class="hero__description">I'm a frontend-end developer and web designer, and I love to create dynamic and fun projects!</p>
+                    <p class="hero__description">Explore this website and learn a bit about me and all the projects I have done until now :)</p>
+                </div>
+            </div>
+
+            <!-- ABOUT -->
+            <div class="about"></div>
+
+            <!-- PROJECTS -->
+            <div class="projects"></div>
+            
+            <!-- CAREER -->
+            <div class="career"></div>
+
+            <!-- BLOG -->
+            <div class="blog"></div>
+
+            <!-- CONTACTS -->
+            <div class="contacts"></div>
+        </div>
+
+        <h2 class="catch-phrase" style="display: none;"></h2>
+
+        <nav class="menu"></nav>
+
+    </main>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
